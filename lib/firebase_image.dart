4bd7909e2bb762d4b2c6adfb1f7d2afb2a1715af
Juan Labrador <juanjavierlabrador@gmail.com>
2024-10-15 10:35:37 -0500

    upgrade flutter 3.24.3, added export firebase core and storage

--- lib/firebase_image.dart
+++ lib/firebase_image.dart
@@ -2,3 +2,5 @@ library firebase_image;
 
 export 'src/firebase_image.dart';
 export 'src/cache_refresh_strategy.dart';
+export 'package:firebase_core/firebase_core.dart';
+export 'package:firebase_storage/firebase_storage.dart';
